771f6d4ca6739126619baad1e7892cc0b4f497d7
youssefbhl <ussefbhl@gmail.com>
2018-05-12 16:35:50 +0200

    ok

--- front/src/js/index.jsx
+++ front/src/js/index.jsx
@@ -32,7 +32,6 @@ class App extends React.Component {
         
           <div>
             <Route exact path="/" component={LoginRect}></Route>
-
             <PrivateRoute path="/test" component={FirstLogin} />
             <Route path="/register" component={Register}></Route>
           </div>
